470b03eac9f51c18d0e078c8cdb7d8996ff12a15
k7i3 <klim.yakovlev@gmail.com>
2015-02-02 18:33:22 +0500

    wip: added some JSF components

--- tnc/web/src/main/java/k7i3/code/helpdesk/tnc/TransportController.java
+++ tnc/web/src/main/java/k7i3/code/helpdesk/tnc/TransportController.java
@@ -1,10 +1,14 @@
 package k7i3.code.helpdesk.tnc;
 
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.inject.Named;
 
 /**
  * Created by k7i3 on 29.01.15.
  */
+@Named
+@RequestScoped
 public class TransportController {
     @Inject
     private TransportEJB transportEJB;
