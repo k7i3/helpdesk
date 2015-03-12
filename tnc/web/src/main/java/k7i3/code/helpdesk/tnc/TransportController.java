3f846e8ca2cc0ecaaa80f7bcfb10322aaa01a5a7
k7i3 <klim.yakovlev@gmail.com>
2015-03-12 18:10:56 +0500

    refactored TransportController (preservation for TransportInfo and TicketInfo to history was added) bug was found (setPropertyActionListener for init didBy doesn't work with AJAX event)

--- tnc/web/src/main/java/k7i3/code/helpdesk/tnc/TransportController.java
+++ tnc/web/src/main/java/k7i3/code/helpdesk/tnc/TransportController.java
@@ -77,6 +77,7 @@ public class TransportController {
 
     public void doAddTransport() {
         LifeCycleInfo lifeCycleInfo = new LifeCycleInfo(new Date(), didBy);
+        //TODO to helper method
         unitOfTransport.setCreation(lifeCycleInfo);
         unitOfTransport.getTransportInfo().setModification(lifeCycleInfo);
         unitOfTransport.getTerminal().setCreation(lifeCycleInfo);
@@ -90,27 +91,67 @@ public class TransportController {
 
     //Do UPDATE
 
-    public void doUpdateTransport() {
-        //TODO save previous Info
-        transportEJB.updateTransport(unitOfTransport);
+    public void doUpdateTransportInfo() { // is used when updating transportEquipment only (as long as)
+        Transport transportForUpdates = transportEJB.findTransportById(unitOfTransport.getId());
 
-        FacesMessage msg = new FacesMessage("Обновлено (комплектация)", unitOfTransport.getTransportInfo().getStateNumber());
+        LifeCycleInfo lifeCycleInfo = new LifeCycleInfo(new Date(), didBy);
+        TransportInfo newTransportInfo = new TransportInfo(unitOfTransport.getTransportInfo());
+        prepareNewTransportInfo(newTransportInfo, lifeCycleInfo);
+        setNewTransportInfo(newTransportInfo, transportForUpdates);
+
+        transportEJB.updateTransport(transportForUpdates);
+
+        FacesMessage msg = new FacesMessage("Обновлено (инфорамция о транспорте)", transportForUpdates.getTransportInfo().getStateNumber());
         FacesContext.getCurrentInstance().addMessage(null, msg);
     }
 
     //AJAX
 
     public void onRowEdit(RowEditEvent event) {
-        //TODO save previous Info
-        transportEJB.updateTransport((Transport) event.getObject());
+        unitOfTransport = (Transport) event.getObject();
+        Transport transportForUpdates = transportEJB.findTransportById(unitOfTransport.getId());
 
-        FacesMessage msg = new FacesMessage("Обновлено (транспорт)", ((Transport) event.getObject()).getTransportInfo().getStateNumber());
+        didBy = "Администратор"; // TODO fix it!
+
+        LifeCycleInfo lifeCycleInfo = new LifeCycleInfo(new Date(), didBy);
+        TransportInfo newTransportInfo = new TransportInfo(unitOfTransport.getTransportInfo()); // TODO make if (equals)
+        prepareNewTransportInfo(newTransportInfo, lifeCycleInfo);
+        setNewTransportInfo(newTransportInfo, transportForUpdates);
+        TerminalInfo newTerminalInfo = new TerminalInfo(unitOfTransport.getTerminal().getTerminalInfo()); // TODO make if (equals)
+        prepareNewTerminalInfo(newTerminalInfo, lifeCycleInfo);
+        setNewTerminalInfo(newTerminalInfo, transportForUpdates);
+
+        transportEJB.updateTransport(transportForUpdates);
+
+        FacesMessage msg = new FacesMessage("Обновлено (информация о транспорте и о терминале)", (transportForUpdates.getTransportInfo().getStateNumber()));
         FacesContext.getCurrentInstance().addMessage(null, msg);
     }
 
     public void onRowCancel(RowEditEvent event) {
         FacesMessage msg = new FacesMessage("Не обновлено (транспорт)", ((Transport) event.getObject()).getTransportInfo().getStateNumber());
         FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+
+    //HELPER METHODS
+
+    private void prepareNewTransportInfo(TransportInfo newTransportInfo, LifeCycleInfo lifeCycleInfo) {
+        newTransportInfo.setModification(lifeCycleInfo);
+    }
+
+    private void setNewTransportInfo(TransportInfo newTransportInfo, Transport transport) {
+        TransportInfo oldTransportInfo = transport.getTransportInfo(); // = new TicketInfo(transport.getTransportInfo());
+        transport.getTransportInfoHistory().add(oldTransportInfo);
+        transport.setTransportInfo(newTransportInfo);
+    }
+
+    private void prepareNewTerminalInfo(TerminalInfo newTerminalInfo, LifeCycleInfo lifeCycleInfo) {
+        newTerminalInfo.setModification(lifeCycleInfo);
+    }
+
+    private void setNewTerminalInfo(TerminalInfo newTerminalInfo, Transport transport) {
+        TerminalInfo oldTerminalInfo = transport.getTerminal().getTerminalInfo(); // = new TicketInfo(transport.getTerminal().getTerminalInfo());
+        transport.getTerminal().getTerminalInfoHistory().add(oldTerminalInfo);
+        transport.getTerminal().setTerminalInfo(newTerminalInfo);
     }
 
 //    public void onCellEdit(CellEditEvent event) {
